e5497d1ad8ad164df0be4bd47ea682bbde8306c5
jjh75607 <dkrak5738@naver.com>
2019-08-20 22:02:51 +0900

    finish Blackjack pt2

--- src/blackJack/BlackjackHand.java
+++ src/blackJack/BlackjackHand.java
@@ -5,9 +5,16 @@ import java.util.ArrayList;
 public class BlackjackHand extends Deck {
 
     public int getValue() {
+        int i = 0;
+        for (Card card : getCards()) {
+            i += new BlackjackCard(card.suitNumber, card.rankNumber).getValue();
 
+            if (i > 21 && card.rankNumber == 1) {
+                i -= 10;
+            }
+        }
 
-        return 21;
+        return i;
     }
 
     public boolean isBusted() {
